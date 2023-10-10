9489a13ed40b486409724627acbb9ce51e80ff6f
YT <wendy199288@gmail.com>
2023-10-10 16:38:50 +0800

    新增專案與gitignore

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float turnSpeed = 50.0f;
+    private float horizontalInput;
+    private float VerticalInput;
+    private float jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //input manager
+        horizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+
+        //control the vehicle move forward and turn
+        //transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * VerticalInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
